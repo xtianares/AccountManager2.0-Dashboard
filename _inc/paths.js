f14ac107d9c30e83d472bd599de044c42650f5ca
xtianares <xtianares@yahoo.com>
2020-05-12 18:11:26 -0400

    started on page account details page

--- _inc/paths.js
+++ _inc/paths.js
@@ -2,7 +2,7 @@
 module.exports = {
   site: {
     src: '_src/',
-    dest: '_dist/'
+    dest: '_build/'
   },
   html: {
     sitePages: '_src/pages/**/*.+(html|njk)',
@@ -13,28 +13,28 @@ module.exports = {
   images: {
     siteFiles: '_src/images/**/*',
     siteFolder: '_src/images/',
-    siteDest: '_dist/images/',
+    siteDest: '_build/images/',
   },
   css: {
     siteFiles: '_src/scss/**/*',
     siteFolder: '_src/scss/',
     siteSass: '_src/scss/main.scss',
-    siteDest: '_dist/css/',
+    siteDest: '_build/css/',
     bsFiles: '_src/bootstrap/scss/**/*',
     bsFolder: '_src/bootstrap/scss/',
     bsSass: '_src/scss/bootstrap/scss/bootstrap.scss',
-    bsDest: '_dist/css/bootstrap'
+    bsDest: '_build/css/bootstrap'
   },
   js: {
     siteFiles: '_src/js/**/*',
     siteRootFiles: '_src/js/*',
     siteFolder: '_src/js/',
-    siteDest: '_dist/js/',
+    siteDest: '_build/js/',
   },
   downloads: {
     files: '_src/downloads/**/*',
     folder: '_src/downloads',
-    dest: '_dist/downloads/'
+    dest: '_build/downloads/'
   },
   root: {
     files: '_src/root/**/*'
